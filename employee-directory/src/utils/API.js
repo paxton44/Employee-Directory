19de0490a83d0e2f03bfbdd620183441737c661e
Mitchell Flood <mitchflood1@gmail.com>
2021-05-11 19:44:03 -0600

    made API using the randomuser api provided in the instructions

---
+++ employee-directory/src/utils/API.js
@@ -0,0 +1,13 @@
+//Activity 19 shows how to use axios and a BASEURL for an api call.
+
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?results=50";
+
+//normally wed need an api key but the provided api is open source and doesn't require a key! 
+// const APIKEY = "&apikey=trilogy";
+
+export default {
+    search: function (query) {
+        return axios.get(BASEURL + query);
+    }
+};
